6a70c3220d59957cd7fcb363dc080b5dd50d2865
gvreergo <gvreergo@gmail.com>
2020-12-25 19:02:52 +0100

    terminé

--- gregs_garage/create_database.py
+++ gregs_garage/create_database.py
@@ -13,28 +13,26 @@ def get_pic_from_directory(DB ,path , label , im_size ):
         im_size : np.array[int]
         _ _ _ _ _ _ _ _ _
         returns DB
-    get every pictures from directory and add them to DB """
-    
+    get every pictures from "path" directory, grayscale,  resize to "im_size" and add them to DB with label "label" """
     dirs = os.listdir(path)
-    #dirs = dirs[:1] # limited in order to speed up testing
-    
+    dirs = dirs[:1] # limited in order to speed up testing
+
     for filename in dirs :
         temp_path = path+'\\'+filename
-        #img treatment 
+        #img treatment
         im=Image.open(temp_path , mode = 'r')
         #formatting
         im = ImageOps.grayscale(im) #to grayscale
         im = np.array (im) #to np.array
         im = resize(im, (im_size[0] , im_size[1]), anti_aliasing=True) #resize to size 256*256
-        plt.imshow(im)
+        #print(im.shape)
         # data normalisation
-        #im /= 255.0 
-        new_row = { 'filename' : filename ,  'label' : label , 'x-ray' : im }
-        #print(new_row)
-        DB = DB.append(new_row , ignore_index=True)
-        #print(DB)
+        #im /= 255.0
+        new_row = pd.DataFrame ([filename ,  label ,  im ]).transpose()
+        #print (new_row)
+        DB = pd.concat([DB , new_row ], ignore_index = True , axis = 0  )
     print(path + " done")
-    
+
     return DB
 
 def create_DB(path ,  label , save_path ,im_size):
@@ -43,16 +41,16 @@ def create_DB(path ,  label , save_path ,im_size):
     label : array[string]
     save_path : str
     im_size : array[int]
-    returns a dataframe of every picture in the repository given in "path" labelled as "label" 
-    /!\/!\/!\/!\/!\/!\data in path[i] wil be labelled with l+abel[i]/!\/!\/!\/!\ """
+    returns a dataframe of every picture in the repository given in "path" labelled as "label"
+    /!\/!\/!\/!\/!\/!\data in path[i] wil be labelled with label[i]/!\/!\/!\/!\ """
     #init values
-    DB = pd.DataFrame( columns = ['filename' , 'label' , 'xray'] )
+    DB = pd.DataFrame()
     length = len(path)
-    
     for i in range(length):
         DB = get_pic_from_directory( DB = DB , path = path[i] , label = label[i] , im_size = im_size )
-    
-    DB.to_csv( path_or_buf = save_path )#, index=True , index_label = [label_1 , label_2] )    
+    DB.to_csv( path_or_buf = save_path )#, index=True , index_label = [label_1 , label_2] )
+    DB.columns = ['filename' , 'label' , 'xray']
+    print(DB)
     return DB
 
 
@@ -65,6 +63,6 @@ if __name__ == "__main__" :
     save_path =  "DB.csv"
     im_size = [256,256]
     DB = create_DB(path , label , save_path , im_size )
+    #del DB[0]
     #print(DB.keys())
-    #print(DB.x_ray[:])
-    #print(DB.index[0])
+    #print(DB.xray[:])
